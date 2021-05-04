449ba3b4cd3a577998ac125cf66e8e603c3180c4
noppe <noppelabs@gmail.com>
2021-05-05 03:41:07 +0900

    Add exit plugin

---
+++ Sources/DebugMenu/Plugin/ExitDebugItem.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by Tomoya Hirano on 2021/05/05.
+//
+
+import Foundation
+
+public struct ExitDebugItem: DebugMenuPresentable {
+    public init() {
+        self.action = .didSelect(action: { (_, _) in
+            exit(0)
+        })
+    }
+    
+    public var debuggerItemTitle: String { "exit" }
+    public let action: DebugMenuAction
+}
