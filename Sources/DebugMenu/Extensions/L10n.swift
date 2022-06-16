ebce4b268b73bc1f3df49d8b53468851a55122da
noppefoxwolf <noppefoxwolf@users.noreply.github.com>
2022-06-16 19:03:57 +0000

    Run swift-format

--- Sources/DebugMenu/Extensions/L10n.swift
+++ Sources/DebugMenu/Extensions/L10n.swift
@@ -5,7 +5,7 @@ extension String {
     static var hideWidget: String { makeLocalizaedString("Hide widget") }
     static var hideUntilNextLaunch: String { makeLocalizaedString("Hide until next launch") }
     static var cancel: String { makeLocalizaedString("Cancel") }
-    
+
     private static func makeLocalizaedString(_ key: String) -> String {
         NSLocalizedString(key, bundle: .module, comment: "")
     }
